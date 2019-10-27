55e2f3bcb31679c908c97c731e9af56ac060c4de
Gabriel B <50279580+gborba-uff-cc@users.noreply.github.com>
2019-10-27 14:33:33 -0300

    Create MapeadorEnderecos.java
    - adicionado: a classe e método para teste de limites

---
+++ MapeadorEnderecos.java
@@ -0,0 +1,13 @@
+package trabalhoso;
+
+public class MapeadorEnderecos{
+
+    public MapeadorEnderecos() {
+    }
+
+    public static dentroLimites(int tamProcesso, int endereçoRelativo) {
+        if (enderecoRelativo > tamProcesso) || (enderecoRelativo < 0) return false;
+        else return true;
+    }
+    
+}
