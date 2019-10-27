1044b4ae4761ad6f44342a6fc3811077ecb9d1a0
Gabriel B <50279580+gborba-uff-cc@users.noreply.github.com>
2019-10-27 17:26:24 -0300

    Update MapeadorEnderecos.java
    - adicionado: Javadoc do método dentroLimites
    - modificado: ecpressão de retorno

--- MapeadorEnderecos.java
+++ MapeadorEnderecos.java
@@ -5,9 +5,15 @@ public class MapeadorEnderecos{
     public MapeadorEnderecos() {
     }
 
-    public static dentroLimites(int tamProcesso, int endereçoRelativo) {
-        if (enderecoRelativo > tamProcesso) || (enderecoRelativo < 0) return false;
-        else return true;
+    /**
+     * Verifica se o endereço está dentro dos limites da IMAGEM do programa.
+     *
+     * @param endRelativo Endereço requisitado no programa.
+     * @param tamImagem Tamanho da imagem do processo em Bytes.
+     * @return true Quando o endereço está dentro da Imagem.
+     */
+    public static boolean dentroLimites(int endRelativo, int tamImagem) {
+        return (endRelativo <= tamImagem) && (endRelativo >= 0);
     }
     
 }
